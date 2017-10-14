01993e41893dae30e97d6bf9e4e114433a389deb
harryli0088 <harryli0088@gmail.com>
2017-10-14 03:40:59 -0500

    barchart

---
+++ imports/ui/pages/home/data.js
@@ -0,0 +1,29 @@
+allergies = [
+  {
+    name:"Claritin",
+    disease:"Seasonal Allergies",
+    efficacy:"71%",
+    price:0.33,
+    rx:"No",
+    years:"15",
+    color:"green"
+  },
+  {
+    name:"Benadryl",
+    disease:"Seasonal Allergies",
+    efficacy:"74%",
+    price:0.36,
+    rx:"No",
+    years:"70",
+    color:"green"
+  },
+  {
+    name:"Zyrtec",
+    disease:"Seasonal Allergies",
+    efficacy:"80%",
+    price:0.68,
+    rx:"No",
+    years:"22",
+    color:"red"
+  }
+]
