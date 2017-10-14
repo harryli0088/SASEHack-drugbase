d2c85cd7aae4bbcdc17e5bc788f1ba40b72b1cfa
harryli0088 <harryli0088@gmail.com>
2017-10-14 04:40:08 -0500

    donut

--- imports/ui/pages/home/data.js
+++ imports/ui/pages/home/data.js
@@ -2,28 +2,28 @@ allergies = [
   {
     name:"Claritin",
     disease:"Seasonal Allergies",
-    efficacy:"71%",
+    efficacy:0.71,
     price:0.33,
     rx:"No",
-    years:"15",
+    years:15,
     color:"#58D68D"
   },
   {
     name:"Benadryl",
     disease:"Seasonal Allergies",
-    efficacy:"74%",
+    efficacy:0.74,
     price:0.36,
     rx:"No",
-    years:"70",
+    years:70,
     color:"#ABEBC6"
   },
   {
     name:"Zyrtec",
     disease:"Seasonal Allergies",
-    efficacy:"80%",
+    efficacy:0.80,
     price:0.68,
     rx:"No",
-    years:"22",
+    years:22,
     color:"#F1948A"
   }
 ]
--- imports/ui/pages/search.js
+++ imports/ui/pages/search.js
@@ -17,6 +17,8 @@ Template.search.rendered = function(){
   var str = Session.get("search").toLowerCase();
 
   barchartPrice(window[str]);
+  barchartEfficacy(window[str]);
+  donut(window[str]);
 };
 
 
@@ -42,7 +44,6 @@ Template.search.helpers({
 Template.search.events({
   "submit form": function(event, template){
     event.preventDefault();
-    console.log("test");
   },
 
   "keydown input": function(event, template){
@@ -68,8 +69,8 @@ Template.search.events({
 barchartPrice = function(data) {
 
   var margin = {top: 40, right: 20, bottom: 30, left: 40},
-  width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 500 - margin.left - margin.right,
+  height = 250 - margin.top - margin.bottom;
 
   var formatPercent = d3.format("$0.00");
 
@@ -95,7 +96,9 @@ barchartPrice = function(data) {
     return "<strong>Price per pill:</strong> <span style='color:"+d.color+"'>" + d.price + "</span>";
   })
 
-  var svg = d3.select("body").append("svg")
+  d3.select("#price").selectAll("*").remove();
+
+  var svg = d3.select("#price").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -121,8 +124,6 @@ barchartPrice = function(data) {
   .style("text-anchor", "end")
   .text("Price per pill");
 
-  console.log(data);
-
   svg.selectAll(".bar")
   .data(data)
   .enter().append("rect")
@@ -133,6 +134,78 @@ barchartPrice = function(data) {
   .attr("height", function(d) { return height - y(d.price); })
   .on('mouseover', tip.show)
   .on('mouseout', tip.hide)
+
+}
+
+barchartEfficacy = function(data) {
+
+  var margin = {top: 40, right: 20, bottom: 30, left: 40},
+  width = 500 - margin.left - margin.right,
+  height = 250 - margin.top - margin.bottom;
+
+  var formatPercent = d3.format("%");
+
+  var x = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1);
+
+  var y = d3.scale.linear()
+  .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .tickFormat(formatPercent);
+
+  var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Efficacy:</strong> <span style='color:"+d.color+"'>" + d.efficacy*100 + "% </span>";
+  })
+
+  d3.select("#efficacy").selectAll("*").remove();
+
+  var svg = d3.select("#efficacy").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  svg.call(tip);
+
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.efficacy; })]);
+
+  svg.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis);
+
+  svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Efficacy");
+
+  svg.selectAll(".bar")
+  .data(data)
+  .enter().append("rect")
+  .attr("fill", function(d) { return d.color; })
+  .attr("x", function(d) { return x(d.name); })
+  .attr("width", x.rangeBand())
+  .attr("y", function(d) { return y(d.efficacy); })
+  .attr("height", function(d) { return height - y(d.efficacy); })
+  .on('mouseover', tip.show)
+  .on('mouseout', tip.hide)
+
 
 }
 
@@ -422,3 +495,54 @@ d3.tip = function() {
 
   return tip
 };
+
+
+
+
+
+
+
+
+donut = function(data) {
+  var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.years; });
+
+var svg = d3.select("#donut").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    console.log(svg);
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.name); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.name; });
+}
+
+function type(d) {
+  d.years = +d.years;
+  return d;
+}
